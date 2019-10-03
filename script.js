b669bea1e1f97f5e1a5bd9e1c1d641576c5e6e14
Anas Qazi <ianasqazi@gmail.com>
2019-10-03 18:46:31 -0400

    Logic working

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 
 // Generate Random Password based on User Input using JavaScript
 
+  
+
+  // var confirmSplChars = true;
+  // var confirmNumChars = false;
+  // var confirmSmlChars = true;
+  // var confirmUprChars = false;
+
+//Function to check if num of characters for Password matches our requirement
+function validateNumChar(){
   window.alert("Please choose minimum 1 option of character set from the following prompt windows");
   var lenPwd = prompt("How many characters do you want in the password ? (8 - 120 range");
 
@@ -8,12 +17,10 @@
   var confirmNumChars = confirm("Do you want to include Numeric characters ? ");
   var confirmSmlChars = confirm("Do you want to include Lower Case characters ? ");
   var confirmUprChars = confirm("Do you want to include Upper Case characters ? ");
-
-//Function to check if num of characters for Password matches our requirement
-function validateNumChar(){
+  
   if ((lenPwd>=8) && (lenPwd<=120)){
-    validateCharSets();
-  } 
+    this.validateCharSets();
+  }
   else{
     alert ("Choose between the range of 8-120 characters !!!")
   }
@@ -23,33 +30,34 @@ function validateNumChar(){
 
 function validateCharSets(){
   if((confirmSplChars || confirmNumChars || confirmSmlChars || confirmUprChars) == true){
-      genPwd(lenPwd);
+   this.genPwd(lenPwd);
   }
   else{
     alert ("Please select one character set minimum !!!")
   }
 }
 
-genSubmit.addEventListener("click",function(e){
-  
-  var pwd = '';
 
-  (confirmSplChars.TRUE) ? pwd +=confirmSplChars : '';
-  (confirmNumChars.TRUE) ? pwd +=confirmNumChars : '';
-  (confirmSmlChars.TRUE) ? pwd +=confirmSmlChars : '';
-  (confirmUprChars.TRUE) ? pwd +=confirmUprChars : '';
-
-  document.getElementById("generatedPassword").innerHTML = genPwd(lenPwd,pwd) ;
-});
 
 //Function to generate Random Password 
-function genPwd (lenPwd,pwd){
- var pwd = '';
- for (var i=0; i<lenPwd; i++){
-   pwd += characters.charAt(Math.floor(Math.random() * pwd.length));
- }
- return pwd;
+
+function genPwd (lenPwd){
+  var pwd = '';
+  var finalPwd = '';
+  var uprChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lwrChars = "abcdefghijklmnopqrstuvwxyz";
+  var numChars = "0123456789";
+  var splChars = "~!@#$%^&*()_+-=`";
+
+  (confirmSplChars==true) ? pwd +=splChars : '';
+  (confirmNumChars==true) ? pwd +=numChars : '';
+  (confirmSmlChars==true) ? pwd +=lwrChars : '';
+  (confirmUprChars==true) ? pwd +=uprChars : '';
+
+ for (var i = 0; i < lenPwd; i++){
+   finalPwd += pwd.charAt(Math.floor(Math.random() * lenPwd));
+ };
+ console.log(finalPwd);
+ document.getElementById('generatedPassword').innerText = finalPwd;
+//  return finalPwd;
 }
-
-
-console.log()
