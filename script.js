878487db9e1058c6d0e88b4866b9f14c689138d7
Anas Qazi <ianasqazi@gmail.com>
2019-10-03 15:29:41 -0400

    validate functions working correctly

--- script.js
+++ script.js
@@ -1,76 +1,55 @@
-// Generate Random Password based on User Input
-var numChars = prompt("How many characters do you want in the password ? ");
-var confirmSplChars = confirm("Do you want to include Special characters ? ");
-var confirmNumChars = confirm("Do you want to include Numeric characters ? ");
-var confirmSmlChars = confirm("Do you want to include Lower Case characters ? ");
-var confirmUprChars = confirm("Do you want to include Upper Case characters ? ");
 
+// Generate Random Password based on User Input using JavaScript
 
+  window.alert("Please choose minimum 1 option of character set from the following prompt windows");
+  var lenPwd = prompt("How many characters do you want in the password ? (8 - 120 range");
 
-function Generate(){
-    // Pasword Length 
-    // Special Characters 
-    // CAPS YES/NO
-    // Display Password
-    document.getElementById("generated-password").value=password;
+  var confirmSplChars = confirm("Do you want to include Special characters ? ");
+  var confirmNumChars = confirm("Do you want to include Numeric characters ? ");
+  var confirmSmlChars = confirm("Do you want to include Lower Case characters ? ");
+  var confirmUprChars = confirm("Do you want to include Upper Case characters ? ");
+
+//Function to check if num of characters for Password matches our requirement
+function validateNumChar(){
+  if ((lenPwd>=8) && (lenPwd<=120)){
+    validateCharSets();
+  } 
+  else{
+    alert ("Choose between the range of 8-120 characters !!!")
+  }
 }
 
-// Function to copy generated password to clipboard
+// Function to check if one of the charactersets have been selected for input
 
-function CopyPwd(){
-    document.getElementById("generated-password").select();
-    document.execCommand("Copy");
-    alert("Password copied to Clipboard !!! ")
+function validateCharSets(){
+  if((confirmSplChars || confirmNumChars || confirmSmlChars || confirmUprChars) == true){
+      genPwd(lenPwd);
+  }
+  else{
+    alert ("Please select one character set minimum !!!")
+  }
 }
 
-{/* <script type="text/javascript">
+genSubmit.addEventListener("click",function(e){
+  
+  var pwd = '';
 
-    var userName = prompt("What's your name?");
-    var confirmCats = confirm("Do you like cats?");
+  (confirmSplChars.TRUE) ? pwd +=confirmSplChars : '';
+  (confirmNumChars.TRUE) ? pwd +=confirmNumChars : '';
+  (confirmSmlChars.TRUE) ? pwd +=confirmSmlChars : '';
+  (confirmUprChars.TRUE) ? pwd +=confirmUprChars : '';
 
-    var petName = "Meeses";
-    var petType = "Tuxedo Cat";
-    var petAge = 5;
-    var petIsCat = true;
+  document.getElementById("generatedPassword").innerHTML = genPwd(lenPwd,pwd) ;
+});
 
-    if (confirmCats === true ) {
-      window.alert(petName + " " + petType + " " + petAge);
-    }
-    else {
-      alert("You don't get my cat's information");
-    }
+//Function to generate Random Password 
+function genPwd (lenPwd,pwd){
+ var pwd = '';
+ for (var i=0; i<lenPwd; i++){
+   pwd += characters.charAt(Math.floor(Math.random() * pwd.length));
+ }
+ return pwd;
+}
 
-    if (petIsCat) {
-      alert("My pet is a cat");
-    }
-    else {
-      alert("My pet is not a cat");
-    }
 
-    if (petAge !== 7) {
-      alert("My pet is not 7 years old");
-    }
-
-    if (petAge === 5) {
-      alert("My pet is 5 years old");
-    }
-    else if (petAge < 5) {
-      alert("My pet is less than 5 years old");
-    }
-    else {
-      alert("My pet is older than 5 years old");
-    }
-
-    // document.write() overwrites the entire page. We don't normally use this, but we will today for simplicity
-    document.write("Welcome to our page " + userName);
-
-    var catAge = prompt("How old is your cat?");
-
-    // Won't work as expected
-    alert(catAge + 2);
-
-    // Works as expected
-    alert(parseInt(catAge) + 2);
-
-    console.log("The output is "+ confirmCats);
-  </script> */}
+console.log()
